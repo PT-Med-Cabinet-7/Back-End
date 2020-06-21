44cf1fe4c0c4158b38593d5b340d4e5f31a52e34
Catherine Kim <catherine@ipwe.com>
2020-06-21 15:46:19 -0700

    Add .env variables and pg code to knexfile and package.json

--- knexfile.js
+++ knexfile.js
@@ -1,28 +1,49 @@
-// Update with your config settings.
-
+// Update with your config settings. 
 module.exports = {
   development: {
-    client: "sqlite3",
+    client: 'sqlite3',
     connection: {
-      filename: "./database/auth.db3",
+      filename: "./database/auth.db3"
     },
     useNullAsDefault: true,
     migrations: {
-      directory: "./database/migrations",
-      tableName: "dbmigrations",
+      directory: './data/migrations',
     },
-    seeds: { directory: "./database/seeds" },
+    seeds: {
+      directory: './data/seeds',
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done)
+      },
+    },
   },
-
   testing: {
-    client: "sqlite3",
-    connection: { filename: "./database/test.db3" },
+    client: 'sqlite3',
+    connection: {
+      filename: "./database/test.db3"
+    },
     useNullAsDefault: true,
     migrations: {
-      directory: "./database/migrations",
-      tableName: "dbmigrations",
+      directory: './data/migrations',
     },
-    seeds: { directory: "./database/seeds" },
+    seeds: {
+      directory: './data/seeds',
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys=ON', done)
+      },
+    },
   },
-  
-};
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: './data/migrations',
+    },
+    seeds: {
+      directory: './data/seeds',
+    },
+  },
+}
